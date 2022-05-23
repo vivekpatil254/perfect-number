87d3424490e6750c1de832957b83e293cb375ec0
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 <Vivekpatil254@gmail.com>
2022-05-23 15:13:13 +0530

    last commit

--- src/com/bridgelabz/Perfect_Number.java
+++ src/com/bridgelabz/Perfect_Number.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Perfect_Number {
     public static void main(String[] args) {
         int sum=0,number,i;
+        System.out.println("Program for to display that the entered number is perfect or not...");
         System.out.println("Enter the Number : ");
         Scanner scanner=new Scanner(System.in);
         number=scanner.nextInt();
@@ -18,9 +19,9 @@ public class Perfect_Number {
 
         }
             if(sum==number)
-                System.out.print("Entered number is a perfect number");
+                System.out.print(number+" is a perfect number");
             else
-                System.out.print("Entered number is not a perfect number");
+                System.out.print(number+" is not a perfect number");
 
     }
 }
